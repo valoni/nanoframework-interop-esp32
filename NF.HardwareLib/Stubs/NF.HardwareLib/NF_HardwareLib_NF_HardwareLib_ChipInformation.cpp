8a1f39c20314cc9ab9b4773becffa61a7ee453ee
Valon M Hoti <valon.hoti@gmail.com>
2020-03-31 16:59:08 +0200

    Add files via upload

---
+++ NF.HardwareLib/Stubs/NF.HardwareLib/NF_HardwareLib_NF_HardwareLib_ChipInformation.cpp
@@ -0,0 +1,85 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+#include "NF_HardwareLib.h"
+#include "NF_HardwareLib_NF_HardwareLib_ChipInformation.h"
+
+using namespace NF_HardwareLib::NF_HardwareLib;
+
+
+void ChipInformation::NativeGetFlashInfo( CLR_RT_TypedArray_UINT8 param0, HRESULT &hr )
+{
+
+    (void)param0;
+    (void)hr;
+
+
+    ////////////////////////////////
+    // implementation starts here //    
+
+
+    // implementation ends here   //    
+    ////////////////////////////////
+
+
+}
+
+void ChipInformation::NativeGetFeatures( CLR_RT_TypedArray_UINT8 param0, HRESULT &hr )
+{
+
+    (void)param0;
+    (void)hr;
+
+
+    ////////////////////////////////
+    // implementation starts here //    
+
+
+    // implementation ends here   //    
+    ////////////////////////////////
+
+
+}
+
+void ChipInformation::NativeGetRevisionNumber( CLR_RT_TypedArray_UINT8 param0, HRESULT &hr )
+{
+
+    (void)param0;
+    (void)hr;
+
+
+    ////////////////////////////////
+    // implementation starts here //    
+
+
+    // implementation ends here   //    
+    ////////////////////////////////
+
+
+}
+
+void ChipInformation::NativeGetChipId( CLR_RT_TypedArray_UINT8 param0, HRESULT &hr )
+{
+
+    (void)param0;
+    (void)hr;
+
+
+    ////////////////////////////////
+    // implementation starts here //    
+
+
+    // implementation ends here   //    
+    ////////////////////////////////
+
+
+}
